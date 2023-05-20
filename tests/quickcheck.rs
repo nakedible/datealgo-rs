e650c34d4ce6d35df562b9a7b8587f2bd5b2effa
Nuutti Kotivuori <naked@iki.fi>
2023-05-20 16:47:59 +0000

    Improve quickchecks

--- tests/quickcheck.rs
+++ tests/quickcheck.rs
@@ -1,19 +1,64 @@
 use datealgo::*;
 
-use chrono::Datelike;
 use quickcheck::{quickcheck, TestResult};
 
 quickcheck! {
-    fn quickcheck_rd_to_date(d: i32) -> TestResult {
-        if d < RD_MIN || d > RD_MAX {
-            return TestResult::discard();
-        }
-        let a = rd_to_date(d);
-        let Some(date) = chrono::NaiveDate::from_num_days_from_ce_opt(d + 719163) else {
-            return TestResult::discard();            
-        };
-        let b = (date.year(), date.month(), date.day());
-        println!("{:?} {:?}", a, b);
+    fn quickcheck_rd_to_date(d: time::Date) -> TestResult {
+        let rd = d.to_julian_day() - 2440588;
+        let a = rd_to_date(rd);
+        let b = (d.year() as i32, d.month() as u32, d.day() as u32);
+        TestResult::from_bool(a == b)
+    }
+
+    fn quickcheck_date_to_rd(d: time::Date) -> TestResult {
+        let a = (d.year() as i32, d.month() as u32, d.day() as u32);
+        let rd = date_to_rd(a);
+        TestResult::from_bool(rd == d.to_julian_day() - 2440588)
+    }
+
+    fn quickcheck_rd_to_weekday(d: time::Date) -> TestResult {
+        let rd = d.to_julian_day() - 2440588;
+        let wd_a = rd_to_weekday(rd);
+        let wd_b = d.weekday().number_from_monday();
+        TestResult::from_bool(wd_a as u8 == wd_b)
+    }
+
+    fn quickcheck_date_to_weekday(d: time::Date) -> TestResult {
+        let rd = d.to_julian_day() - 2440588;
+        let a = rd_to_date(rd);
+        let wd_a = date_to_weekday(a);
+        let wd_b = d.weekday().number_from_monday();
+        TestResult::from_bool(wd_a as u8 == wd_b)
+    }
+
+    fn quickcheck_systemtime_to_datetime(s: time::PrimitiveDateTime) -> TestResult {
+        let s = s.assume_utc();
+        let a = systemtime_to_datetime(s.into()).unwrap();
+        let b = (
+            s.year() as i32,
+            s.month() as u32,
+            s.day() as u32,
+            s.hour() as u8,
+            s.minute() as u8,
+            s.second() as u8,
+            s.nanosecond(),
+        );
+        TestResult::from_bool(a == b)
+    }
+
+    fn quickcheck_datetime_to_systemtime(s: time::PrimitiveDateTime) -> TestResult {
+        let s = s.assume_utc();
+        let dt = (
+            s.year() as i32,
+            s.month() as u32,
+            s.day() as u32,
+            s.hour() as u8,
+            s.minute() as u8,
+            s.second() as u8,
+            s.nanosecond(),
+        );
+        let a = datetime_to_systemtime(dt);
+        let b: std::time::SystemTime = s.into();
         TestResult::from_bool(a == b)
     }
 }
