11ca5cdf0d043f34f2f756086f2c9e8506289a3d
Nuutti Kotivuori <naked@iki.fi>
2023-09-02 13:49:47 +0000

    chore: Change basic benchmark to use random values

--- benches/basic.rs
+++ benches/basic.rs
@@ -1,44 +1,119 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use std::time::{Duration, UNIX_EPOCH};
+use std::time::{Duration, Instant, SystemTime};
+
+fn bencher<I: Copy, O>(s: impl Fn() -> I, f: impl Fn(I) -> O) -> impl Fn(u64) -> Duration {
+    move |n| {
+        let v = s();
+        let now = Instant::now();
+        for _ in 0..n {
+            let _ = black_box(f(v));
+        }
+        now.elapsed()
+    }
+}
+
+fn rand_year() -> i32 {
+    fastrand::i32(datealgo::YEAR_MIN..=datealgo::YEAR_MAX)
+}
+
+fn rand_rd() -> i32 {
+    fastrand::i32(datealgo::RD_MIN..=datealgo::RD_MAX)
+}
+
+fn rand_date() -> (i32, u8, u8) {
+    let y = rand_year();
+    let m = fastrand::u8(1..=12);
+    let d = fastrand::u8(1..=datealgo::days_in_month(y, m));
+    (y, m, d)
+}
+
+fn rand_secs() -> i64 {
+    fastrand::i64(datealgo::RD_SECONDS_MIN..=datealgo::RD_SECONDS_MAX)
+}
+
+fn rand_hms() -> (u8, u8, u8) {
+    let h = fastrand::u8(0..=23);
+    let m = fastrand::u8(0..=59);
+    let s = fastrand::u8(0..59);
+    (h, m, s)
+}
+
+fn rand_dhms() -> (i32, u8, u8, u8) {
+    let rd = rand_rd();
+    let (h, m, s) = rand_hms();
+    (rd, h, m, s)
+}
+
+fn rand_dt() -> (i32, u8, u8, u8, u8, u8) {
+    let (y, m, d) = rand_date();
+    let (hh, mm, ss) = rand_hms();
+    (y, m, d, hh, mm, ss)
+}
+
+fn rand_ym() -> (i32, u8) {
+    let y = rand_year();
+    let m = fastrand::u8(1..=12);
+    (y, m)
+}
+
+fn rand_sn() -> (i64, u32) {
+    let s = rand_secs();
+    let n = fastrand::u32(0..=999_999_999);
+    (s, n)
+}
+
+fn rand_st() -> SystemTime {
+    datealgo::secs_to_systemtime(rand_sn()).unwrap()
+}
+
+fn rand_dtn() -> (i32, u8, u8, u8, u8, u8, u32) {
+    let (y, m, d, hh, mm, ss) = rand_dt();
+    let n = fastrand::u32(0..=999_999_999);
+    (y, m, d, hh, mm, ss, n)
+}
 
 fn bench_basic(c: &mut Criterion) {
-    let rd = datealgo::date_to_rd((2023, 5, 12));
-    c.bench_function("rd_to_date", |b| b.iter(|| black_box(datealgo::rd_to_date(black_box(rd)))));
-    let d = (2023, 5, 12);
-    c.bench_function("date_to_rd", |b| b.iter(|| black_box(datealgo::date_to_rd(black_box(d)))));
-    c.bench_function("rd_to_weekday", |b| b.iter(|| black_box(datealgo::rd_to_weekday(black_box(rd)))));
-    c.bench_function("date_to_weekday", |b| b.iter(|| black_box(datealgo::date_to_weekday(black_box(d)))));
-    let s = 1684574678i64;
-    c.bench_function("secs_to_dhms", |b| b.iter(|| black_box(datealgo::secs_to_dhms(black_box(s)))));
-    let dhms = (123123, 12, 34, 56);
-    c.bench_function("dhms_to_secs", |b| b.iter(|| black_box(datealgo::dhms_to_secs(black_box(dhms)))));
+    c.bench_function("rd_to_date", |b| {
+        b.iter_custom(bencher(rand_rd, |rd| datealgo::rd_to_date(black_box(rd))))
+    });
+    c.bench_function("date_to_rd", |b| {
+        b.iter_custom(bencher(rand_date, |d| datealgo::date_to_rd(black_box(d))))
+    });
+    c.bench_function("rd_to_weekday", |b| {
+        b.iter_custom(bencher(rand_rd, |rd| datealgo::rd_to_weekday(black_box(rd))))
+    });
+    c.bench_function("date_to_weekday", |b| {
+        b.iter_custom(bencher(rand_date, |d| datealgo::date_to_weekday(black_box(d))))
+    });
+    c.bench_function("secs_to_dhms", |b| {
+        b.iter_custom(bencher(rand_secs, |s| datealgo::secs_to_dhms(black_box(s))))
+    });
+    c.bench_function("dhms_to_secs", |b| {
+        b.iter_custom(bencher(rand_dhms, |dhms| datealgo::dhms_to_secs(black_box(dhms))))
+    });
     c.bench_function("secs_to_datetime", |b| {
-        b.iter(|| black_box(datealgo::secs_to_datetime(black_box(s))))
+        b.iter_custom(bencher(rand_secs, |s| datealgo::secs_to_datetime(black_box(s))))
     });
-    let dt = (2023, 5, 20, 12, 34, 56);
     c.bench_function("datetime_to_secs", |b| {
-        b.iter(|| black_box(datealgo::datetime_to_secs(black_box(dt))))
+        b.iter_custom(bencher(rand_dt, |dt| datealgo::datetime_to_secs(black_box(dt))))
     });
-    let y = 2000;
-    c.bench_function("is_leap_year", |b| b.iter(|| black_box(datealgo::is_leap_year(black_box(y)))));
-    let m = 2;
+    c.bench_function("is_leap_year", |b| {
+        b.iter_custom(bencher(rand_year, |y| datealgo::is_leap_year(black_box(y))))
+    });
     c.bench_function("days_in_month", |b| {
-        b.iter(|| black_box(datealgo::days_in_month(black_box(y), black_box(m))))
+        b.iter_custom(bencher(rand_ym, |(y, m)| datealgo::days_in_month(black_box(y), black_box(m))))
     });
-    let st = UNIX_EPOCH + Duration::from_secs(1684574678);
     c.bench_function("systemtime_to_secs", |b| {
-        b.iter(|| black_box(datealgo::systemtime_to_secs(black_box(st))))
+        b.iter_custom(bencher(rand_st, |st| datealgo::systemtime_to_secs(black_box(st))))
     });
-    let sn = (1684574678, 0);
     c.bench_function("secs_to_systemtime", |b| {
-        b.iter(|| black_box(datealgo::secs_to_systemtime(black_box(sn))))
+        b.iter_custom(bencher(rand_sn, |sn| datealgo::secs_to_systemtime(black_box(sn))))
     });
     c.bench_function("systemtime_to_datetime", |b| {
-        b.iter(|| black_box(datealgo::systemtime_to_datetime(black_box(st))))
+        b.iter_custom(bencher(rand_st, |st| datealgo::systemtime_to_datetime(black_box(st))))
     });
-    let dtn = (2023, 5, 20, 12, 34, 56, 0);
     c.bench_function("datetime_to_systemtime", |b| {
-        b.iter(|| black_box(datealgo::datetime_to_systemtime(black_box(dtn))))
+        b.iter_custom(bencher(rand_dtn, |dtn| datealgo::datetime_to_systemtime(black_box(dtn))))
     });
 }
 
