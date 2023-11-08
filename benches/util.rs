a7fd6d52e519235a3adc8d5872c019fd8cbd0e63
Nuutti Kotivuori <naked@iki.fi>
2023-11-08 12:12:16 +0000

    chore: More reliable benchmarks by array input
    Previous benchmarking method picked a random value and repeatedly
    tested the same value for each sample, leading to a case where
    performance differences due to different input values were marked
    as outliers.
    
    Current method pregenerates a random array of values to be processed
    so each sample will contain a mix of input values, leading to them
    being averaged based on how often they appear in the randomized data.
    
    This will cause an increase of 50 ps for overhead, which is not
    relevant for our benchmarking.

--- benches/util.rs
+++ benches/util.rs
@@ -2,11 +2,13 @@ use criterion::black_box;
 use std::time::{Duration, Instant};
 
 pub fn bencher<I: Copy, O>(s: impl Fn() -> I, f: impl Fn(I) -> O) -> impl Fn(u64) -> Duration {
+    const ARR_SIZE: usize = 4096;
     move |n| {
-        let v = s();
+        fastrand::seed(7);
+        let is: [I; ARR_SIZE] = std::array::from_fn(|_| s());
         let now = Instant::now();
-        for _ in 0..n {
-            let _ = black_box(f(v));
+        for i in 0..n {
+            let _ = black_box(f(is[i as usize & (ARR_SIZE-1)]));
         }
         now.elapsed()
     }
